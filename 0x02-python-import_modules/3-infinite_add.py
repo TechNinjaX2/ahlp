fa273821989818ba3ea56ea304691d264520621a
shadrack <phoenixnova427@gmail.com>
2024-09-14 19:37:14 +0300

    3 now executes properly

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -9,13 +9,10 @@ if __name__ == '__main__':
             continue
         else:
             array[index - 1] = i
-            print("{}".format(array[index - 1]))
             index = index + 1
     a = 0
     index = 0
-    for j in array[index]:
-        print(f"{array[index]}")
+    while (index != len(array)):
         a = a + int(array[index])
         index = index + 1
-    print("{}".format(array[2]))
     print("{}".format(a))
