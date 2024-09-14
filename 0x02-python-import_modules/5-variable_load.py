ebf1b6e21f446bea36cb8f26e783834f0474502b
shadrack <phoenixnova427@gmail.com>
2024-09-14 19:08:49 +0300

    2nd 5 import changed

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-import variable_load_5#as var
 if __name__ == '__main__':
-    print("{}".format(variable_load_5.a))
+    import variable_load_5 as var
+    #if __name__ == '__main__':
+    print("{}".format(var.a))
