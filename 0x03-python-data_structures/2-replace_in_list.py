5e174f4040c9284a16dbf4f483663acd0910568d
shadrack <phoenixnova427@gmail.com>
2024-09-17 17:11:12 +0300

    adjusted if parameters

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     size = len(my_list)
-    if (idx > size):
+    if (idx > size - 1):
         return my_list
     elif (idx < 0):
         return my_list
