9cc44df3f3afc76e7484df5989437132a87bcff2
shadrack <phoenixnova427@gmail.com>
2024-09-17 17:08:23 +0300

    adjusted if parameters

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     size = len(my_list)
-    if (idx > size):
+    if (idx > size - 1):
         return None
     elif (idx < 0):
         return None
