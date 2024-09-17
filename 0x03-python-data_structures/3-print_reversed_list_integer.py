af2832a031f656b7d1563ac545ced333ac6aba11
shadrack <phoenixnova427@gmail.com>
2024-09-17 17:24:19 +0300

    attempt code fix for empty string

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     size = len(my_list)
-    while (size > 0):
-        size = size - 1
-        print("{:d}".format(my_list[size]))
+    if (size != 0):
+        while (size > 0):
+            size = size - 1
+            print("{:d}".format(my_list[size]))
