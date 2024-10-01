ee287dfcc25e7bcbd407137fca413b35170d89ae
shadrack <phoenixnova427@gmail.com>
2024-10-01 03:20:04 +0300

    added except block

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,12 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
-    try:
-        while (i < x):
+    count = 0
+    while (i < x):
+        try:
             print("{}".format(my_list[i]), end="")
-            i += 1
-    finally:
-        print("")
-        return i
+            count += 1
+        except IndexError:
+            pass
+        i += 1
+    print("")
+    return count
